263bd7266a067b3e1cf7e3baf06f12ecdbdd171a
Evan Freeman <59422750+evan-freeman@users.noreply.github.com>
2021-02-06 20:27:28 -0800

    began this

---
+++ neural_net.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+
+
+class NeuralNet:
+    def __init__(self, x, y):
+        self.input = x
+        self.y = y
+        self.output = np.zeros(y.shape)
+
+        self.input_layers = x.shape[1]
+        self.hidden_layers = 2
+        self.output_layers = y.shape[1]
+
+        self.input = x
+        self.weights1 = np.random.rand(self.input_layers, self.hidden_layers)
+        self.weights2 = np.random.rand(self.hidden_layers, self.output_layers)
+
+        # We're setting biases = 0 for now
+
+    def feedforward(self):
+        self.layer1 = sigmoid
